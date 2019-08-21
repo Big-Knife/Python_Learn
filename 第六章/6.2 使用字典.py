c5eff17827e3e6759f2ed73c41b911b86e37f64a
BigKnife <45336540+Big-Knife@users.noreply.github.com>
2019-08-21 15:03:32 +0800

    initial commit

--- 第六章/6.2 使用字典.py
+++ 第六章/6.2 使用字典.py
@@ -2,3 +2,13 @@ alien_0={'color':'green','points':5}
 #获取键的值
 new_points=alien_0['points']
 print("你的得分"+str(new_points))
+
+alien_0['x_position']=0
+alien_0['y_position']=25
+print(alien_0)
+
+#空字典添加键值
+alien_0={}
+alien_0['color']='green'
+alien_0['points']=5
+print(alien_0)
