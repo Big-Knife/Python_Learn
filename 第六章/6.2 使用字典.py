a3328b7291dbb034dcfd993aa762ae91fd66b030
BigKnife <45336540+Big-Knife@users.noreply.github.com>
2019-08-21 15:27:33 +0800

    initial commit

--- 第六章/6.2 使用字典.py
+++ 第六章/6.2 使用字典.py
@@ -12,3 +12,15 @@ alien_0={}
 alien_0['color']='green'
 alien_0['points']=5
 print(alien_0)
+
+#修改字典中的值
+alien_0={'x_position':5,'y_position':25,'speed':'medium'}
+print("当前x轴位置:"+str(alien_0['x_position']))
+if alien_0['speed']=='slow':
+    x_increment=1
+elif alien_0['speed']=='medium':
+    x_increment=2
+else:
+    x_increment=3
+alien_0['x_position']=alien_0['x_position']+x_increment
+print("新的x轴坐标位置："+str(alien_0['x_position']))
