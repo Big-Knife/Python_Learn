e104422a1d61e61dc5a2b5194372a36a7c906f34
BigKnife <45336540+Big-Knife@users.noreply.github.com>
2019-08-26 14:09:57 +0800

    initial commit

---
+++ 第七章/7-8~10练习.py
@@ -0,0 +1,20 @@
+# 熟食店
+'''sandwiche_orders=['原味','加辣','海鲜']
+finished_sandwiches=[]
+while sandwiche_orders:
+    current=sandwiche_orders.pop()
+    finished_sandwiches.append(current)
+for finished_sandwiche in finished_sandwiches:
+    print(finished_sandwiche)'''
+
+# 五香烟熏牛肉
+sandwich_orders = [
+    'pastrami', 'veggie', 'grilled cheese', 'pastrami',
+    'turkey', 'roast beef', 'pastrami']
+finished_sandwiches = []
+
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove('pastrami')
+
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
---
+++ 第八章/8.1 定义函数.py
@@ -0,0 +1,4 @@
+def greet_user(username):
+    '''显示简单的问侯句'''
+    print("Hello" + username.title()+"!")
+greet_user(' PY')
