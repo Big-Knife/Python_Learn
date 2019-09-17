2a983d12720e95fd1a2e0b3eb9655419cc8fbf9f
BigKnife <45336540+Big-Knife@users.noreply.github.com>
2019-09-17 21:07:28 +0800

    initial commit

--- 第九章/9.2 使用类和实例.py
+++ 第九章/9.2 使用类和实例.py
@@ -6,6 +6,41 @@ class Car():
         self.year = year
 
     def get_descriptive_name(self):
-        long_name = str(self.year) + self.make + self.modle
+        long_name = str(self.year) +' ' + self.make + ' ' + self.modle
         return long_name.title()
 
+my_new_car = Car('audi','a4','2016')
+print(my_new_car.get_descriptive_name())
+
+
+class Car():
+
+    def __init__(self,make,modle,year):
+        self.make = make
+        self.modle = modle
+        self.year = year
+        self.odometer_reading = 0
+
+    def get_descriptive_name(self):
+        long_name = str(self.year) +' ' + self.make + ' ' + self.modle
+        return long_name.title()
+
+    def read_odometer(self):
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+    def update_odometer(self,mileage):
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+my_new_car = Car('audi','a4','2016')
+print(my_new_car.get_descriptive_name())
+#直接修改属性的值
+my_new_car.odometer_reading = 27
+my_new_car.read_odometer()
+
+#通过方法修改属性的值
+my_new_car = Car('audi','a4','2016')
+my_new_car.update_odometer(23)
+my_new_car.read_odometer()
