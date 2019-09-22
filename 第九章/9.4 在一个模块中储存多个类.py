846f4459c884a725aab4f13bd2e19d9be9770f4a
BigKnife <45336540+Big-Knife@users.noreply.github.com>
2019-09-22 23:31:01 +0800

    innn

--- 第九章/9.4 在一个模块中储存多个类.py
+++ 第九章/9.4 在一个模块中储存多个类.py
@@ -23,6 +23,22 @@ class Car():
     def increment_odometer(self,miles):
         self.odomemter_reading += miles
 
+class Battery():
+    '''一次模拟电动汽车电瓶的属性'''
+    def __init__(self,battery_size=70):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        '''打印一条描述电瓶容量的消息'''
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def get_range(self):
+        '''打印一条描述电瓶续航里程的消息'''
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
 class ElectricCar(Car):
     '''电动汽车的独特之处'''
 
