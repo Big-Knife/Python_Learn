ba332f29ad9626baf90aff4e59e3ec9b53c13a81
BigKnife <45336540+Big-Knife@users.noreply.github.com>
2019-09-23 13:25:56 +0800

    initial commit

--- 第九章/9.4 在一个模块中储存多个类.py
+++ 第九章/9.4 在一个模块中储存多个类.py
@@ -1,60 +1,7 @@
-class Car():
-    '''一次模拟汽车的简单尝试'''
+from car import ElectricCar
 
-    def __init__(self,make,model,year):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odomemter_reading = 0
+my_tesla = ElectricCar('tesla','model',2016)
 
-    def get_descripive_name(self):
-        long_name = str(self.year) + ' ' + self.make + ' ' + self.model
-        return long_name
-
-    def read_odometer(self):
-        print("This car has " + str(self.odomemter_reading) + " miles on it.")
-
-    def update_odometer(self,mileage):
-        if mileage >= self.odomemter_reading:
-            self.odomemter_reading = mileage
-        else:
-            print("You can't roll back an odometer!")
-
-    def increment_odometer(self,miles):
-        self.odomemter_reading += miles
-
-class Battery():
-    '''一次模拟电动汽车电瓶的属性'''
-    def __init__(self,battery_size=70):
-        self.battery_size = battery_size
-
-    def describe_battery(self):
-        '''打印一条描述电瓶容量的消息'''
-        print("This car has a " + str(self.battery_size) + "-kWh battery.")
-
-    def get_range(self):
-        '''打印一条描述电瓶续航里程的消息'''
-        if self.battery_size == 70:
-            range = 240
-        elif self.battery_size == 85:
-            range = 270
-
-class ElectricCar(Car):
-    '''电动汽车的独特之处'''
-
-    def __init__(self,make,model,year):
-        '''初始化父类的属性'''
-        super().__init__(make,model,year)
-        self.battery_size = 70
-
-    def describe_battery(self):
-        '''描述电池容量的信息'''
-        print("This car has a " + str(self.battery_size) + "-kWh battery.")
-
-    def fill_gas_tank():
-        '''ElectricCar Not Fill'''
-        print("This car doesn't need a gas tank!")
-
-my_tesla = ElectricCar('tesla','model s',2016)
-print(my_tesla.get_descripive_name())
-my_tesla.describe_battery()
+print(my_tesla.get_descriptive_name())
+my_tesla.battery.describe_battery()
+my_tesla.battery.get_range()
--- 第九章/car.py
+++ 第九章/car.py
@@ -22,3 +22,31 @@ class Car():
 
     def increment_odometer(self,miles):
         self.odomemter_reading += miles
+
+class Battery():
+    '''一次模拟电动汽车电瓶的属性'''
+    def __init__(self,battery_size=70):
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        '''打印一条描述电瓶容量的消息'''
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def get_range(self):
+        '''打印一条描述电瓶续航里程的消息'''
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+
+class ElectricCar(Car):
+    '''电动汽车的独特之处'''
+
+    def __init__(self,make,model,year):
+        '''初始化父类的属性'''
+        super().__init__(make,model,year)
+        self.battery = Battery
