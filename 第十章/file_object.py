6d00c5c15f56abd345bc9bb3f9ce91e29e63b5fa
BigKnife <45336540+Big-Knife@users.noreply.github.com>
2019-09-30 00:04:23 +0800

    innn

--- 第十章/file_object.py
+++ 第十章/file_object.py
@@ -1,3 +1,14 @@
-with open('pi_digits.txt') as file_object:
+with open('text_files\pi_digits.txt') as file_object:
+    #使用反斜杠(\)设置路径 让程序在该目录子文件夹打开指定文件
     content = file_object.read()
     print(content.rstrip())
+
+#每次一行的方式检查文件
+
+file_name = 'text_files\pi_digits.txt'
+
+with open(file_name) as file_object:
+    for line in file_object:
+        #用rstrip()删除内容空格
+        print(line.rstrip())
+
