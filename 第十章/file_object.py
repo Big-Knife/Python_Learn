daa19eae55822ff33c429666f4a59245d1ed107b
BigKnife <45336540+Big-Knife@users.noreply.github.com>
2019-10-01 09:18:02 +0800

    Update file_object.py

--- 第十章/file_object.py
+++ 第十章/file_object.py
@@ -3,6 +3,8 @@ with open('text_files\pi_digits.txt') as file_object:
     content = file_object.read()
     print(content.rstrip())
 
+print('\n')
+
 #每次一行的方式检查文件
 
 file_name = 'text_files\pi_digits.txt'
@@ -10,6 +12,28 @@ file_name = 'text_files\pi_digits.txt'
 with open(file_name) as file_object:
     for line in file_object:
         #用rstrip()删除内容空格
-        print(line.rstrip())
+        print(line.rstrip() )
 
-###s
+print('\n')
+
+#创建一个包含文件各行内容的列表
+file_name = 'text_files\pi_digits.txt'
+
+with open(file_name) as file_object:
+    lines = file_object.readlines()
+
+for line in lines:
+    print(line.rstrip())
+
+#使用文件的内容
+file_name = 'text_files\pi_digits.txt'
+
+with open(file_name) as file_object:
+    lines = file_object.readlines()
+
+pi_string = ''
+for line in lines:
+    pi_string += line.rstrip()
+
+print(pi_string)
+print(len(pi_string))
