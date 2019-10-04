799fbedeef368727466bc4ff333a6f458b55ddcd
BigKnife <45336540+Big-Knife@users.noreply.github.com>
2019-10-04 16:17:42 +0800

    innn

--- 第十章/file_object.py
+++ 第十章/file_object.py
@@ -24,16 +24,3 @@ with open(file_name) as file_object:
 
 for line in lines:
     print(line.rstrip())
-
-#使用文件的内容
-file_name = 'text_files\pi_digits.txt'
-
-with open(file_name) as file_object:
-    lines = file_object.readlines()
-
-pi_string = ''
-for line in lines:
-    pi_string += line.rstrip()
-
-print(pi_string)
-print(len(pi_string))
