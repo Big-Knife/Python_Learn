7e226ad530454179bcfacf60227b3a1c5d14fa1c
BigKnife <45336540+Big-Knife@users.noreply.github.com>
2019-10-10 22:22:53 +0800

    initial commit

--- 第十章/练习2.py
+++ 第十章/练习2.py
@@ -1,6 +1,20 @@
+#访客
+# 编写一个程序，提示用户输入其名字后；用户做出响应
+# 后，将其名字写入到文件guest.txt中。#
+
 filename = 'text_files\guest.txt'
 
 name = input("what's your name?")
 
 with open(filename,'w') as file_object:
     file_object.write(name)
+
+filename = 'text_files\guest_book.txt'
+
+print("Enter 'quit' when you are finished.")
+while Ture:
+    name = input("\n What's your name?")
+    if name == 'quit'
+        break
+    else::
+    with open(filename,)
