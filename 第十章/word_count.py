61c3608c5bc5f9c6747b13a94c5ffb301279685e
BigKnife <45336540+Big-Knife@users.noreply.github.com>
2019-10-15 22:39:32 +0800

    默认

--- 第十章/word_count.py
+++ 第十章/word_count.py
@@ -1,5 +1,16 @@
-filename = 'alice.txt'
-
 def count_words(filename):
     '''计算一个文件大致包含多少个单词'''
     try:
+        with open(filename) as f_obj:
+            countents = f_obj.read()
+    except FileNotFoundError:
+        pass
+    else:
+        #计算文件大致包含多少个单词
+        words= countents.split()
+        num_words = len(words)
+        print("The file " + filename + " has about " + str(num_words) + " words.")
+
+filenames = ['alice.txt','siddhartha.txt','moby_dick.txt','little_women.txt']
+for filename in  filenames:
+    count_words(filename)
