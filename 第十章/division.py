6d0c2ca3d88b5d4c14a16c3c5f42c493c21610f9
BigKnife <45336540+Big-Knife@users.noreply.github.com>
2019-10-15 21:40:42 +0800

    Initial commit

---
+++ 第十章/division.py
@@ -0,0 +1,44 @@
+try:
+    print(5/0)
+except ZeroDivisionError:
+    print("You can't dicide by zero!")
+
+print("Give me two numbers,and i'll divide them.")
+print("Enter 'q' to quit.")
+
+while True:
+    first_number = input("\nFirst number:")
+    if first_number == 'q':
+        break
+    second_number = input("\nSecond number:")
+    if second_number == 'q':
+        break
+    try:
+        answer = int(first_number)/int(second_number)
+    except ZeroDivisionError:
+        print("You can't divide by 0!")
+    else:
+        print(answer)
+
+
+filename = 'alice.txt'
+try:
+    with open(filename) as f:
+        contents = f.read()
+except FileNotFoundError:
+    msg = "Sorry,the file " + filename + " does not exist."
+    print(msg)
+
+filename = 'alice.txt'
+
+try:
+    with open(filename) as f_obj:
+        contents = f_obj.read()
+except FileNotFoundError:
+    msg = "Sorry,the file " + filename + " does not exist."
+    print(msg)
+else:
+    #计算文件大致包含多少个单词
+    words = contents.split()
+    num_words = len(words)
+    print("The file " + filename + " has about " + str(num_words) + " words.")
---
+++ 第十章/word_count.py
@@ -0,0 +1,5 @@
+filename = 'alice.txt'
+
+def count_words(filename):
+    '''计算一个文件大致包含多少个单词'''
+    try:
