84d230f5b659c82d05bd0152e8005eab4ca15c1d
BigKnife <45336540+Big-Knife@users.noreply.github.com>
2020-01-02 00:04:56 +0800

    默认

--- Alien_Pygame/alien_invasion.py
+++ Alien_Pygame/alien_invasion.py
@@ -8,6 +8,8 @@ from ship import Ship
 
 import game_functions as gf
 
+from pygame.sprite import Group
+
 
 def run_game():
     # 初始化游戏并创建一个屏幕对象
@@ -19,7 +21,9 @@ def run_game():
     pygame.display.set_caption("Alien Invasion")
 
     # 创建一艘飞船
-    ship = Ship(screen)
+    ship = Ship(ai_settings, screen)
+    # 创建一个用于存储子弹的编组
+    bullets = Group()
 
     # 设置背景色
     bg_color = (230, 230, 230)
@@ -27,10 +31,10 @@ def run_game():
     # 开始游戏的主循环
     while True:
         # 监视键盘和鼠标事件
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         # 每次循环时都重绘屏幕
-        gf.update_screen(ai_settings, screen, ship)
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
         # 让最近绘制的屏幕可见
         pygame.display.flip()
