ec4a0c195b6c411ac6ded6ac4c7a610f65939b12
AITGEEK <45336540+Big-Knife@users.noreply.github.com>
2020-01-06 14:14:57 +0800

    Initial commit

--- Alien_Pygame/ship.py
+++ Alien_Pygame/ship.py
@@ -26,7 +26,7 @@ class Ship():
 
     def update(self):
         """根据移动标志调整飞船位置"""
-        # 更新飞船的center数值，而不是rect
+        # 更新飞船的center值，而不是rect
         if self.moving_right and self.rect.right < self.screen_rect.right:
             self.center += self.ai_settings.ship_speed_factor
         if self.moving_left and self.rect.left > 0:
